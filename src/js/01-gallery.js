1f28520c9b080f56bed8e2afeac4ad90817e0096
Irina Skopetskaya <sairina2704@gmail.com>
2021-12-30 13:44:10 +0200

    tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,37 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const gallery = document.querySelector('.gallery');
+const imageMarcup = createGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imageMarcup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('');
+    
+};
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('');
+};
+let image = '';
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay:250,});
+lightbox.on('show.simplelightbox', function (e) {
+    image = `<div class = "modal"><img src = "${e.target.dataset.source}"</div>`;
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit' , onFormSubmit);
+document.addEventListener('DOMContentLoaded', checked);
+
+
+function onFormInput(e){
+    const mail = input.value;
+    const message = textarea.value;
+    const feedbackFormData = {mail, message, };
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormData));
+ };
+function onFormSubmit(e){
+    e.preventDefault();
+    e.currentTarget.reset();
+    const mail = input.value;
+    const message = textarea.value;
+    const feedbackFormData = {mail, message, };
+    const saved = JSON.parse(localStorage.getItem('feedback-form-state'));
+    console.log(saved);
+    localStorage.removeItem('feedback-form-state');
+};
+function checked() {
+    const savedInput = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if(savedInput){
+       input.value = savedInput.mail;
+        textarea.value = savedInput.message;
+    }
+   };
+
 
